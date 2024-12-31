1e0bc56147777712bc34f40f1cd8eac368dfd26e
Jeevanandam Ponnusamy <ponnusamy.jeevanandam@ing.com>
2024-12-31 22:44:54 +0000

    narendra

--- models/Dharma_learn/employee.sql
+++ models/Dharma_learn/employee.sql
@@ -8,3 +8,5 @@ union
 select 3 as employee_id,'karthik' as employee_name,40 as age from dual
 union
 select 4 as employee_id,'Prabha' as employee_name,25 as age from dual
+union
+select 5 as employee_id,'Narendra' as employee_name,50 as age from dual
